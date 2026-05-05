9a5ee8eb0d77b141d29854d6fd11ab19b6ecdd3c
cetinfuat45-cmyk <cetinfuat45@gmail.com>
2026-05-05 07:55:10 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'bakim-tv-v1';
+const ASSETS = [
+  'planl-bkmtv.html',
+  'manifest.json',
+  'pwa_icon_512_1777956810565.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate and Clean Old Caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Assets from Cache or Network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
